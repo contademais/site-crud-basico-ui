4bbd48025dff9a1c2a73d95d6ed5aff2a530fe43
contademais <adsonvon12@gmail.com>
2025-06-21 19:00:04 -0300

    Alterado link da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://site-crud-basico.vercel.app",
+  baseURL: "https://site-crud-basico-api.vercel.app/",
 });
 
 export default api;
